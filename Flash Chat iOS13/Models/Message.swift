bbc17a530b1bb242cda16029edcd3518dd1c0b6a
dai954 <da0606ha@gmail.com>
2021-03-17 22:00:40 +0900

    third_commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 石川大輔 on 2021/03/16.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
